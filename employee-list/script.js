e0c860958daebaf9f1250a47f57335bf1b6b3133
Lantao-bit <lantao1965@gmail.com>
2025-12-05 05:13:53 +0000

    Render function

---
+++ employee-list/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // define main function to create some employees    
+    function main() {
+        // add employees using the functions from data.js   
+        addEmployee(employees, "Alice Brown", "Data Scientist");
+        addEmployee(employees, "Bob White", "DevOps Engineer");
+        addEmployee(employees, "Charlie Green", "Product Owner");
+    };
+
+    // call main function to create employees
+    main();
+
+    //define a function to render the employee list in the DOM
+    function renderEmployeeList(employees) {
+        let employeeList = document.getElementById("employee-list");
+        employeeList.innerHTML = ""; // Clear the existing list
+
+        for ( let employee of employees ) {
+            const listItem = document.createElement("li");
+            listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+            listItem.innerHTML = `${employee.id}: ${employee.name} <span class="badge bg-primary"> ${employee.position}`;
+            employeeList.appendChild(listItem);
+        } 
+    }
+
+    // Initial render of the employee list
+    renderEmployeeList();
+
+});
