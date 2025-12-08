722fb851330717be7da12c192253f7f38b26dd70
Lantao-bit <lantao1965@gmail.com>
2025-12-08 10:15:34 +0000

    clean desk

---
+++ 05-assessment/data.js
@@ -0,0 +1,23 @@
+let employees = [
+    {
+        id: 1,
+        name: "Alice Johnson",
+        gender: "Female",
+        position: "Software Engineer",
+        salary: 90000
+    },
+    {
+        id: 2,
+        name: "Bob Smith",
+        gender: "Male",
+        position: "Marketing Manager",
+        salary: 75000
+    },
+    {
+        id: 3,
+        name: "Charlie Davis",
+        gender: "Male",
+        position: "Sales Executive",
+        salary: 60000
+    }
+];
---
+++ 05-assessment/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    renderEmployeeList(employees);
+});
+
+function renderEmployeeList(employees) {
+    let tableBody = document.querySelector('#employeeTableBody');
+    tableBody.innerHTML = '';
+
+    let newRow = document.createElement('tr');
+
+    for (let employee of employees) {
+
+        newRow.innerHTML = `
+           <td> ${employee.id}</td>
+           <td> ${employee.name}</td>
+           <td> ${employee.gender}</td>            
+           <td> ${employee.position}</td>
+           <td> ${employee.salary}</td> 
+           <td>
+              <button class="btn btn-sm btn-warning edit-btn">Edit</button>
+              <button class="btn btn-sm btn-danger delete-btn">Delete</button>
+           </td>
+       `;
+       tableBody.appendChild(newRow);
+    }
+};
